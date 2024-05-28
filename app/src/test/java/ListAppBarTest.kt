4ae649bf55f158ff933a93a3849e750cd4f6f531
nishantkumar <nishant.mca2010@gmail.com>
2024-05-28 14:21:29 +0530

    Added unit test case for search icon

--- app/src/test/java/ListAppBarTest.kt
+++ app/src/test/java/ListAppBarTest.kt
@@ -1,6 +1,7 @@
-import android.os.Build
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import com.singlepoint.todo.data.ToDoDao
 import com.singlepoint.todo.data.models.Priority
@@ -8,15 +9,14 @@ import com.singlepoint.todo.data.models.ToDoTask
 import com.singlepoint.todo.data.repository.DataStoreRepository
 import com.singlepoint.todo.data.repository.ToDoRepository
 import com.singlepoint.todo.ui.screens.list.DefaultListAppBar
+import com.singlepoint.todo.ui.screens.list.SearchAction
 import com.singlepoint.todo.ui.viewmodels.SharedViewModel
 import com.singlepoint.todo.util.Action
 import com.singlepoint.todo.util.SearchAppBarState
 import kotlinx.coroutines.flow.flowOf
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
-import org.mockito.Mockito.mockStatic
 import org.mockito.Mockito.`when`
 
 class ListAppBarTest {
@@ -24,16 +24,8 @@ class ListAppBarTest {
     @get:Rule
     val composeRule = createComposeRule()
 
-    @Before
-    fun setUp() {
-        // Set up a mock for android.os.Build
-        mockStatic(Build::class.java)
-        `when`(Build.FINGERPRINT).thenReturn("mockFingerprint")
-        `when`(Build.MODEL).thenReturn("mockModel")
-    }
-
     @Test
-    fun testAppBar() {
+    fun testDefaultListAppBar() {
 
         // Mock the ToDoDao
         val mockToDoDao = mock(ToDoDao::class.java)
@@ -50,8 +42,6 @@ class ListAppBarTest {
         // Create an instance of the SharedViewModel with the repository
         val sharedViewModel = SharedViewModel(toDoRepository, mockDataStoreRepository)
 
-
-
         composeRule.setContent {
             DefaultListAppBar(
                 onSearchClicked = {
@@ -65,8 +55,16 @@ class ListAppBarTest {
                 }
             )
         }
+        composeRule.onNodeWithText(text = "Tasks").assertTextEquals("Tasks")
+    }
 
-        //composeRule.onNodeWithContentDescription("Search").assertIsDisplayed()
-        composeRule.onNodeWithText(text = "Tasks").assertIsDisplayed()
+    @Test
+    fun testSearchAction() {
+        composeRule.setContent {
+           SearchAction {
+
+           }
+        }
+        composeRule.onNodeWithContentDescription("Search Tasks").assertIsDisplayed()
     }
 }
